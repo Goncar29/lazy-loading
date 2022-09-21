9bad6ad1ffb6743ebff2ebd56248bcb2a1167567
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-09-21 16:45:38 -0300

    Creamos el proyecto con una api para mostrar imagenes con click en boton

--- src/index.js
+++ src/index.js
@@ -4,3 +4,35 @@
  **/
 
 console.log('Happy hacking :)')
+
+const min = 1
+const max = 123
+const random = () => Math.floor(Math.random() * (max - min) + min)
+
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = 'p-4';
+
+    const imagen = document.createElement('img');
+    imagen.className = 'mx-auto';
+    imagen.width = '320';
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('images');
+
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+};
+
+addButton.addEventListener('click', addImage);
+
+
