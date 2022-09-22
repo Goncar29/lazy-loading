d4783ad5d8a2bbe8be618f6b37d99475e93958c1
Goncar29 <Goncar29@users.noreply.github.com>
2022-09-22 20:05:15 +0000

    deploy: 8b54d5a7c5dbc148dad899f1611b4d09504bc2f4

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -15,8 +15,13 @@ const cargarImagen = (entry) => {
 
 const observer = new IntersectionObserver((entries) => {
     entries.filter(isIntersecting).forEach(cargarImagen);
+    console.log('carga')
 });
 
 export const registerImage = (imagen) => {
     observer.observe(imagen)
 };
+
+//queda por hacer un boton "limpiar" de todas las imagenes que carguemos
+// realizar que aparezca un cuadro gris en el lugar donde aparece la imagen
+// que apaerzca en consola la cantidad de imagenes que se han cargado y total de imagenes que se agregaron
